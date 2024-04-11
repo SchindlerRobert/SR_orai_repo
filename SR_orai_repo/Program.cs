c6568f796a85f0dfe202fe215b89b00abcb8d142
User <User@DESKTOP-74AOQJK>
2024-04-11 11:34:31 +0200

    Add project files.

---
+++ SR_orai_repo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR_orai_repo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
